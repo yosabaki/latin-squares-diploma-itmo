623debbad2d6b32bfb650321ef24f60b71390420
Artem Ustinov <ustinov.artem1@huawei.com>
2021-05-19 02:17:00 +0300

    add run incremental script

---
+++ runIncremental.sh
@@ -0,0 +1,12 @@
+export LD_LIBRARY_PATH=./cryptominisat/build/lib
+
+ENCODER="java -jar ./latin-square-generator/build/libs/latin-square-generator-1.0-SNAPSHOT.jar";
+INCREMENTAL="incremental/incremental"
+encode="encode -e reduced_latin_onehot -i";
+decode="decode -e reduced_latin_onehot -o";
+
+inputs="inputs";
+outputs="outputs";
+
+$ENCODER $encode "$inputs/incrementalInput" -n 10 -k 3 -r 1;
+$INCREMENTAL "$inputs/incrementalInput" "$outputs/incremental/incrementalOutput" $1;
